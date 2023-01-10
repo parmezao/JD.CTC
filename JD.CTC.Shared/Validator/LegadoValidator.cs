7d645a3c3eb7b2c2f67c370da6df081bff42a17a
parmezao <wagner@parmex.com.br>
2023-01-10 12:52:48 -0300

    Add FluentValidation

---
+++ JD.CTC.Shared/Validator/LegadoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using JD.CTC.Shared.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JD.CTC.Shared.Validator
+{
+    public class LegadoValidator : AbstractValidator<Legado>
+    {
+        public LegadoValidator()
+        {
+            RuleFor(x => x.CdLegado).NotEmpty().WithMessage("*campo obrigatório");
+            RuleFor(x => x.NomeLegado).NotEmpty().WithMessage("*campo obrigatório");
+            RuleFor(x => x.SitLegado).NotEmpty().WithMessage("*campo obrigatório");
+        }
+    }
+}
